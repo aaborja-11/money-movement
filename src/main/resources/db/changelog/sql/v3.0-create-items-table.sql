7120c73a6baf7ce1fe6a72b35cce2b27053f0776
Alfredo Borja Jr <aaborja11@gmail.com>
2025-07-10 15:51:50 +0800

    Add items and inventory table with relationship

---
+++ src/main/resources/db/changelog/sql/v3.0-create-items-table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE mm.items (
+    id UUID PRIMARY KEY,
+    business_id UUID NOT NULL,
+    CONSTRAINT fk_business
+        FOREIGN KEY (business_id)
+        REFERENCES business(id)
+        ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL,
+    unit_measurement VARCHAR(25) NOT NULL,
+    cost_price DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    selling_price DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    active BOOLEAN NOT NULL DEFAULT FALSE,
+    type VARCHAR(50) NOT NULL,
+    created_by VARCHAR(255) NOT NULL DEFAULT 'mm',
+    created_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_by VARCHAR(255) NOT NULL DEFAULT 'mm',
+    updated_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
---
+++ src/main/resources/db/changelog/sql/v4.0-create-inventory-table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE mm.inventory (
+    id UUID PRIMARY KEY,
+    item_id UUID NOT NULL,
+    CONSTRAINT fk_item
+        FOREIGN KEY (item_id)
+        REFERENCES items(id)
+        ON DELETE CASCADE,
+    opening_stock DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    remaining_stock DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    opening_timestamp TIMESTAMP,
+    closing_timestamp TIMESTAMP,
+    created_by VARCHAR(255) NOT NULL DEFAULT 'mm',
+    created_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_by VARCHAR(255) NOT NULL DEFAULT 'mm',
+    updated_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
