49dfc73d1c3465d48181a81a42232d24177eab00
Alfredo Borja Jr <aaborja11@gmail.com>
2025-07-11 12:38:36 +0800

    Create transactions table

---
+++ src/main/resources/db/changelog/sql/v6.0-create-transactions-table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE mm.transactions (
+    id UUID PRIMARY KEY,
+    item_id UUID NOT NULL,
+    CONSTRAINT fk_item
+        FOREIGN KEY (item_id)
+        REFERENCES items(id)
+        ON DELETE CASCADE,
+    purchased_amount DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    purchased_quantity DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    purchased_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    type VARCHAR(75) NOT NULL,
+    created_by VARCHAR(255) NOT NULL DEFAULT 'mm',
+    created_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_by VARCHAR(255) NOT NULL DEFAULT 'mm',
+    updated_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
