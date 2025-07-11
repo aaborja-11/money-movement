d990ead2cfcbf036ad52a6c84a7b1f049902bfd6
Alfredo Borja Jr <aaborja11@gmail.com>
2025-07-11 11:58:32 +0800

    add sales table

---
+++ src/main/resources/db/changelog/sql/v5.0-create-sales-table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE mm.sales (
+    id UUID PRIMARY KEY,
+    item_id UUID NOT NULL,
+    CONSTRAINT fk_item
+        FOREIGN KEY (item_id)
+        REFERENCES items(id)
+        ON DELETE CASCADE,
+    income DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    expense DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    loss DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    profit DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    opening_timestamp TIMESTAMP,
+    closing_timestamp TIMESTAMP,
+    created_by VARCHAR(255) NOT NULL DEFAULT 'mm',
+    created_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_by VARCHAR(255) NOT NULL DEFAULT 'mm',
+    updated_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
